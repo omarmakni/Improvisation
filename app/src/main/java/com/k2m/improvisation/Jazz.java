c80e5b1dbcacdd66583a32fc0e4c9ed311b77f8e
omarmakni <omarmakni1@gmail.com>
2017-04-09 08:43:50 +0200

    zidouhaa

---
+++ app/src/main/java/com/k2m/improvisation/Jazz.java
@@ -0,0 +1,75 @@
+package com.k2m.improvisation;
+
+/**
+ * Created by omarm on 09/04/2017.
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+import com.cooltechworks.views.ScratchImageView;
+
+
+public class Jazz extends Activity
+{
+    Button button;
+    RadioButton kitkat, lollipop, marshmallow;
+    RadioGroup radioGroup;
+    ScratchImageView scratchImageView;
+
+   protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.jazz);
+
+       button = (Button) findViewById(R.id.button);
+       scratchImageView = (ScratchImageView) findViewById(R.id.scratchView1);
+
+       radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+       kitkat = (RadioButton) findViewById(R.id.KitKat);
+       lollipop = (RadioButton) findViewById(R.id.Lollipop);
+       marshmallow = (RadioButton) findViewById(R.id.Marshmallow);
+
+       scratchImageView.setRevealListener(new ScratchImageView.IRevealListener() {
+           @Override
+           public void onRevealed(ScratchImageView scratchImageView) {
+
+               //Don't Write Any Code Here
+           }
+       });
+
+       button.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+
+               if (!scratchImageView.isRevealed()) {
+
+                   Toast.makeText(Jazz.this, "Please Scratch The Whole Image First", Toast.LENGTH_LONG).show();
+
+               } else {
+
+                   switch (radioGroup.getCheckedRadioButtonId()) {
+
+                       case R.id.Lollipop:
+                           Toast.makeText(Jazz.this, "Wrong Captcha Entered.", Toast.LENGTH_LONG).show();
+                           break;
+
+                       case R.id.Marshmallow:
+                           Toast.makeText(Jazz.this, "Wrong Captcha Entered.", Toast.LENGTH_LONG).show();
+                           break;
+
+                       case R.id.KitKat:
+                           Toast.makeText(Jazz.this, "You Have Entered Correct Captcha Code.", Toast.LENGTH_LONG).show();
+                           break;
+                   }
+               }
+           }
+       });
+
+    }
+}
